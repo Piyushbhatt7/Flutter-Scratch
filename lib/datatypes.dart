70f76ef50843830a12526fd3ee834becbb3ca2f3
vengersmoon <vengersmon003@gmail.com>
2024-11-24 15:16:16 +0530

    value assign

--- lib/datatypes.dart
+++ lib/datatypes.dart
@@ -4,4 +4,7 @@ void main() {
    
    int? a;
    print(a); // a contain null value so it return the null!
+
+   int b = 5;
+   print(b); // assign the right hand side value to left hand side variable!
 }
