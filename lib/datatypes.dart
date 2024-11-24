ee5bab07230464cd9f2813367628b74c9767eef5
vengersmoon <vengersmon003@gmail.com>
2024-11-24 15:24:59 +0530

    BigInt

--- lib/datatypes.dart
+++ lib/datatypes.dart
@@ -9,4 +9,10 @@ void main() {
    print(b); // assign the right hand side value to left hand side variable!
 
    String name = "Lion"; // inline variable declaration
+
+   int x = 54434423435456654345; // int cannot store this value
+
+   BigInt longValue;
+
+   longValue = BigInt.parse('54434423435456654345'); // to store the largest value we use the BigInt
 }
