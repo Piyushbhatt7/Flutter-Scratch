d1d383f660d74dc9e753df7c360d874a40be918e
vengersmoon <vengersmon003@gmail.com>
2024-11-24 21:53:51 +0530

    function

--- lib/function.dart
+++ lib/function.dart
@@ -3,11 +3,17 @@ class myClass{
   void func(){ // function declaration [void return to type]
     print("Peoples Life");
   }
+
+  void func1(String name){ // function with parameter
+       print(name);
+  }
 }
 
 void main(){
 
   myClass obj =myClass(); // class object decleration
   obj.func(); // function caling
+  obj.func1("Are good"); // passing argument to a function
+
 
 }
