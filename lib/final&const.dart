d9ea2d7b0df1448705973bcb89d93c6c0e786c87
vengersmoon <vengersmon003@gmail.com>
2024-11-25 22:36:31 +0530

    final and const

---
+++ lib/final&const.dart
@@ -0,0 +1,9 @@
+void main(){
+
+    final name = "Pigsus"; // only once the value is declared  
+    final name1;
+    name1 = "Dynamoo"; // final can be assign later
+    print(name1);
+    print(name);
+
+}
