38e974ee112b335af8f58a4cd14776d82dff22c7
vengersmoon <vengersmon003@gmail.com>
2024-11-25 09:44:31 +0530

    List in dart

---
+++ lib/list.dart
@@ -0,0 +1,10 @@
+Dart List is similar to an array, which is the ordered collection of the objects.
+
+The array is the most popular and commonly used collection in any other programming language
+
+The synatax of list is:-
+
+                          var list = [1,2,3,4,5,6];
+                          [] = list is define using the square bracket
+
+The Drt list is defined by storing all elements inside a [] = square bracket and seprated the each elements by ","
