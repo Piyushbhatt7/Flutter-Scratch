5c9125dcd34a6644aebc911bfc2b95452000ce27
vengersmoon <vengersmon003@gmail.com>
2024-11-25 10:13:16 +0530

    list insert

--- lib/list_insert.dart
+++ lib/list_insert.dart
@@ -1,6 +1,9 @@
 void main() {
    
    var name = ['Ronaldo', 'Messi', 'Ramos', 'Toni Kross'];
+   var number = [7, 10, 11, 4, 8];
    name.insert(2, "Neymar"); // inserting element to index 2 [ index starts = 0 1 2 3 ...... n ]
+   
    print(name);
+
 }
