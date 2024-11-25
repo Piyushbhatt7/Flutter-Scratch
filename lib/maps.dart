9a2ecfc7de8e98d88c7e5af6852873aa1d0d1eee
vengersmoon <vengersmon003@gmail.com>
2024-11-25 11:50:51 +0530

    Map in dart

---
+++ lib/maps.dart
@@ -0,0 +1,11 @@
+void main(){
+   
+   var map_name = {
+
+      'Key1' : 'Value1',       
+      'Key2' : 'Value2',
+      'Key3' : 'Value3',        // key must be unique value can be duplicate
+      'Key4' : 'Value4',
+
+   };
+}
