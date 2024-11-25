487efa646bfefed8a5a1853c81595953b9ce8ff2
vengersmoon <vengersmon003@gmail.com>
2024-11-25 10:02:06 +0530

    List in dart

--- lib/list.dart
+++ lib/list.dart
@@ -3,4 +3,8 @@ void main(){
   var list = [1,2,3,4,5]; // list decelaration
   list.add(6); // add the element in the next or last index we use .add
   print(list); // print the list element [1,2,3,4,5]
+
+  var list1 = []; // blank all
+  list1.addAll(list); // .addAll use to add all elements
+  print(list1);
 }
