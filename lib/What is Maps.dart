da27067c011a34f718d3006ebdbd87cf14d9a162
vengersmoon <vengersmon003@gmail.com>
2024-11-25 11:35:37 +0530

    Maps

---
+++ lib/What is Maps.dart
@@ -0,0 +1,12 @@
+                                     What is Map in Dart Programming ?
+
+
+In Dart Programming, Maps are Dictionary-like data types that exist in key-value form(know as lock-key)
+
+There is no restriction on the type of data that goes in a map data type.
+
+Maps are very flexible and can mutate their size based on the requirements.
+
+It is important to note that all locks(keys) need to be unique a map data type.
+
+var map_name = {key1:value1, key2:value1, ... }
