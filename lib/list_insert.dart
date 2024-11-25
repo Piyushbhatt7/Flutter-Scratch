7c5c9cf6fcc98caa590b369afaf65dad40c4d622
vengersmoon <vengersmon003@gmail.com>
2024-11-25 10:18:09 +0530

    list insert

--- lib/list_insert.dart
+++ lib/list_insert.dart
@@ -1,9 +1,9 @@
 void main() {
    
-   var name = ['Ronaldo', 'Messi', 'Ramos', 'Toni Kross'];
+   List <dynamic> name = ['Ronaldo', 'Messi', 'Ramos', 'Toni Kross'];
    var number = [7, 10, 11, 4, 8];
    name.insert(2, "Neymar"); // inserting element to index 2 [ index starts = 0 1 2 3 ...... n ]
-   
+   name.insertAll(5, number); // inserAll add the all element of another list but the list shoul be "dynamic"!
    print(name);
 
 }
