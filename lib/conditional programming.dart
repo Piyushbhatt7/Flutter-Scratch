65dc2aa28a128dce85cf2c1a6831ca529d169de3
vengersmoon <vengersmon003@gmail.com>
2024-11-26 09:19:33 +0530

    conditional programming

--- lib/conditional programming.dart
+++ lib/conditional programming.dart
@@ -2,10 +2,10 @@ void main(){
    
    var a = 100;
 
-   if(a>200){
+   if(a>200){  // check 100 is greater than 200 if yes print true
       print(true);
    }
    else{
-      print(false);
+      print(false);  
    }
 }
