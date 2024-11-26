793ae19e8e04c6bddcb9b13111a2fb2f4ae964ce
vengersmoon <vengersmon003@gmail.com>
2024-11-26 11:50:40 +0530

    while loop

--- lib/while loop.dart
+++ lib/while loop.dart
@@ -1,8 +1,10 @@
 void main(){
    
-   var number = 100;
+   var number = 10;
    
-   while(number<50){
+   while(number<=50){
       print('Number is = $number'); // if the condition is false it will not executes
+      number++;
+     // number+=1;
    }
 }
