1f6c23f85de1cc1a478d914e13015fbde1c50762
vengersmoon <vengersmon003@gmail.com>
2024-11-26 09:24:24 +0530

    else if statement

---
+++ lib/elseif.dart
@@ -0,0 +1,15 @@
+void main(){
+   
+   var a = 100;
+
+   if(a>200){
+     // condition 1 is true
+   }
+
+   else if(a<50){
+      // condition 2 is true
+   }
+   else if(a>80) {
+      // condition 3 is true
+   }
+}
