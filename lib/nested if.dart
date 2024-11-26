6796b0b6cd898ce7c6ee76adf69a165122b559c5
vengersmoon <vengersmon003@gmail.com>
2024-11-26 09:32:04 +0530

    nested if statement

---
+++ lib/nested if.dart
@@ -0,0 +1,13 @@
+void main(){
+   
+   var a = 100;
+   var b = 50;
+
+   if(a>200){
+    // condition 1
+
+    if(b>100){                   //also write like
+      // when both if are true
+    }
+   }
+}
